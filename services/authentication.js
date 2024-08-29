e4a136473fd3794557d91c6bb6d1f75314fb2056
Ritesh Ranjan <ranjanritesh13102001@gmail.com>
2024-08-29 07:22:44 +0530

    added vercel.json

--- services/authentication.js
+++ services/authentication.js
@@ -1,6 +1,6 @@
+require("dotenv").config();
 const JWT = require("jsonwebtoken");
-
-const secret = "$uperMan@123";
+const secret = process.env.JWT_SECRET;
 
 function createTokenForUser(user) {
   const payload = {
