c4ee4dbf53278e0e746cfed932c5b56721b64065
Ritesh Ranjan <84141214+Ritesh-1310@users.noreply.github.com>
2024-12-16 18:39:24 +0530

    Remove assetlinks.json

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ const cookieParser = require("cookie-parser");
 const Blog = require("./models/blog");
 const userRoute = require("./routes/user");
 const blogRoute = require("./routes/blog");
-const assetLinks = require('./public/assetlinks.json');
 
 const {
   checkForAuthenticationCookie,
@@ -41,10 +40,6 @@ app.get("/", async (req, res) => {
     console.error("Error fetching blogs:", error);
     res.status(500).render("error", { message: "Failed to fetch blogs" });
   }
-});
-
-app.get('/.well-known/assetlinks.json', (req, res) => {
-  res.json(assetLinks); // Send the assetlinks.json content as a response
 });
 
 app.use("/user", userRoute);
